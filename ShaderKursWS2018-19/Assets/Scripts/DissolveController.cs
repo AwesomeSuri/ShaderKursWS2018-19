a8ce6d6ed569e69fe497b1c9400f0a63f6618e40
AwesomeSuri <akbar.suriaganda@gmail.com>
2019-01-31 11:20:09 +0100

    Dissolve script

---
+++ ShaderKursWS2018-19/Assets/Scripts/DissolveController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveController : MonoBehaviour
+{
+    public Material matDissolve;
+    public Vector4 dissolve;
+
+    private void OnValidate()
+    {
+        matDissolve.SetColor("_Dissolve", new Color(dissolve.x, dissolve.y, dissolve.z, dissolve.w));
+    }
+}
